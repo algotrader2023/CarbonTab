bd11c48479a087684c54fa78b357d7c0a31e05bb
algotrader2023 <borntocode8@gmail.com>
2024-05-08 15:47:36 +0200

    Add project files.

---
+++ CarbonTab/CarbonTabControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CarbonTab
+{
+    /// <summary>
+    /// A custom TabControl implementation
+    /// </summary>
+    public class CarbonTabControl : TabControl
+    {
+        /// <summary>
+        /// Returns a new instance of <see cref="CarbonTabControl"/>
+        /// </summary>
+        static CarbonTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CarbonTabControl), new FrameworkPropertyMetadata(typeof(CarbonTabControl)));
+        }
+
+        /// <summary>
+        /// Update the left and right tab item
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            int itemCount = Items.Count;
+            int index = 0;
+            foreach(object tabItem in Items)
+            {
+                if(tabItem is CarbonTabItem)
+                {
+                    CarbonTabItem _tabItem = tabItem as CarbonTabItem;
+                    if (index == 0)
+                    {
+                        _tabItem.IsFirst = true;
+                        _tabItem.IsLast = false;
+                    }
+                    else if (index == (itemCount - 1))
+                    {
+                        _tabItem.IsFirst = false;
+                        _tabItem.IsLast = true;
+                    }
+                    else
+                    {
+                        _tabItem.IsFirst = _tabItem.IsLast = false;
+                    }
+                }
+                index++;
+            }
+        }
+
+
+        public HorizontalAlignment TabPanelHAlign
+        {
+            get { return (HorizontalAlignment)GetValue(TabPanelHAlignProperty); }
+            set { SetValue(TabPanelHAlignProperty, value); }
+        }
+
+        public VerticalAlignment TabPanelVAlign
+        {
+            get { return (VerticalAlignment) GetValue(TabPanelVAlignProperty); }
+            set { SetValue(TabPanelVAlignProperty, value); }
+        }
+
+        public Brush SelectedTabBrush
+        {
+            get { return (Brush)GetValue(SelectedTabBrushProperty); }
+            set { SetValue(SelectedTabBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TabPanelHAlign.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TabPanelHAlignProperty =
+            DependencyProperty.Register(nameof(TabPanelHAlign), typeof(HorizontalAlignment), typeof(CarbonTabControl), new PropertyMetadata(defaultValue: HorizontalAlignment.Left));
+
+
+        // Using a DependencyProperty as the backing store for TabPanelHAlign.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TabPanelVAlignProperty =
+            DependencyProperty.Register(nameof(TabPanelVAlign), typeof(VerticalAlignment), typeof(CarbonTabControl), new PropertyMetadata(defaultValue: VerticalAlignment.Top));
+                
+
+        // Using a DependencyProperty as the backing store for SelectedTabBrush.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedTabBrushProperty =
+            DependencyProperty.Register(nameof(SelectedTabBrush), typeof(Brush), typeof(CarbonTabControl), new PropertyMetadata(defaultValue: Brushes.Gray));
+
+    }
+}
---
+++ CarbonTab/CarbonTabItem.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CarbonTab
+{
+    /// <summary>
+    /// A single tab page inside the <see cref="CarbonTabControl"/>
+    /// </summary>
+    public class CarbonTabItem : TabItem
+    {
+        /// <summary>
+        /// Returns a new instance of <see cref="CarbonTabItem"/>
+        /// </summary>
+        static CarbonTabItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CarbonTabItem), new FrameworkPropertyMetadata(typeof(CarbonTabItem)));
+        }
+
+        /// <summary>
+        /// Returns true if this tab item is the first in the tab control
+        /// </summary>
+        public bool IsFirst
+        {
+            get => (bool)GetValue(IsFirstProperty);
+            set => SetValue(IsFirstProperty, value);
+        }
+
+        /// <summary>
+        /// Returns true if this tab item is the last in the tab control
+        /// </summary>
+        public bool IsLast
+        {
+            get => (bool)GetValue(IsLastProperty);
+            set => SetValue(IsLastProperty, value);
+        }
+
+        /// <summary>
+        /// Returns corner radius to apply to the 
+        /// </summary>
+        public double CornerRadius
+        {
+            get => (double) GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for IsFirst.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsFirstProperty =
+            DependencyProperty.Register(nameof(IsFirst), typeof(bool), typeof(CarbonTabItem), new PropertyMetadata(defaultValue: true));
+
+        // Using a DependencyProperty as the backing store for IsLast.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsLastProperty =
+            DependencyProperty.Register(nameof(IsLast), typeof(bool), typeof(CarbonTabItem), new PropertyMetadata(defaultValue: true));
+
+        // Using a DependencyProperty as the backing store for CornerRadius.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(double), typeof(CarbonTabItem), new PropertyMetadata(default(double)));
+    }
+}
